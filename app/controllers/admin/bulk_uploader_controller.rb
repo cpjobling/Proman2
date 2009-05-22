876ffe68cb16d9430c39a9835b6ba922f795ddd7
Dr Chris P. Jobling <C.P.Jobling@Swansea.ac.uk>
2009-05-22 18:32:17 +0100

    Really is working now

--- app/controllers/admin/bulk_uploader_controller.rb
+++ app/controllers/admin/bulk_uploader_controller.rb
@@ -12,7 +12,6 @@ class Admin::BulkUploaderController < ApplicationController
    end
 
    def csv_import_staff
-   	   logger = Logger.new STDOUT 
    	   staff_role = Role.find_by_name('staff')
        @parsed_file = CSV::Reader.parse(params[:import_staff][:staff_file])
        n=0
@@ -20,21 +19,28 @@ class Admin::BulkUploaderController < ApplicationController
        @parsed_file.each  do |row|
            user = User.new
            logger.info "Read: #{row}"
+           # 0: title, 1: first, 2: initials, 3: last, 4: staff_id, 5: login
            user.id = row[4].to_i * 1000000 # staff number shifted 6 digits to left
                                            # to avoid clash with 6 digit student number
            user.login = row[5]
-           user_name = Name.new(row[1], row[2], row[0], row[3], "")
+
+           title = row[0] || ""
+           first_name = row[1] || ""
+           initials = row[2] || ""
+           last_name = row[3] || ""
+           user_name = Name.new(first_name, last_name, title, initials, "")
+           
            user.name = user_name
            user.password = 'swansea' + row[4]
            user.password_confirmation = 'swansea' + row[4]
            user.email = row[5] + '@swansea.ac.uk' # login@swansea.ac.uk
            user.add_role(staff_role)
            if user.save
-           	 result = result + "#{n}: Added #{user_name} as #{user.id}<br />\n"
+           	 logger.info "#{n}: Added #{user_name} as #{user.id}<br />\n"
              n = n+1
              GC.start if n % 50 == 0
            else
-              flash.now[:error] = "Couldn't save record for #{user.name}"
+              logger.error "Couldn't save record for #{user.name}"
             end
            flash[:notice] = "#{result}\nCSV Staff Import Successful,  #{n} new records added to data base."
       end
