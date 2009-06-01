173d5bf7af8da01903730743ebf93fcee527083b
Dr Chris P. Jobling <C.P.Jobling@Swansea.ac.uk>
2009-06-01 14:58:52 +0100

    Supervisor/Students show and edit complete

--- app/models/student.rb
+++ app/models/student.rb
@@ -13,7 +13,7 @@
 #   limitations under the License.
 
 class Student < ActiveRecord::Base
-  has_one :user
+  belongs_to :user
   belongs_to :discipline
   has_one :project, :through => :project_allocation
   has_many :selections, :through => :project_selections, :source => :project
