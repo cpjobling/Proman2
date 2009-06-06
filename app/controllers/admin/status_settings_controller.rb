b5bd01011b893ae476af3b0ce27f8a656b8a3b07
Dr Chris P. Jobling <C.P.Jobling@Swansea.ac.uk>
2009-06-06 20:08:57 +0100

    New and edit pages

--- app/controllers/admin/status_settings_controller.rb
+++ app/controllers/admin/status_settings_controller.rb
@@ -21,7 +21,72 @@ class Admin::StatusSettingsController < ApplicationController
 
     respond_to do |format|
       format.html # show.html.erb
-      format.xml  { render :xml => @project }
+      format.xml  { render :xml => @status_setting }
     end
   end
+
+  # GET /admin/status_settings/new
+  # GET /admin/status_settings/new.xml
+  def new
+    @setting = StatusSetting.new
+
+
+    respond_to do |format|
+      format.html # new.html.erb
+      format.xml  { render :xml => @setting }
+    end
+  end
+
+  # GET /admin/status_settings/1/edit
+  def edit
+    @setting = StatusSetting.find(params[:id])
+  end
+
+  # POST /admin/status_settings
+  # POST /admin/status_settings.xml
+  def create
+    @status_setting = StatusSetting.new(params[:status_setting])
+
+    respond_to do |format|
+      if @status_setting.save
+        flash[:notice] = 'status_setting was successfully created.'
+        format.html { redirect_to(admin_status_setting_path(@status_setting)) }
+        format.xml  { render :xml => @status_setting, :status => :created, :location => @status_setting }
+      else
+        format.html { render :action => "new" }
+        format.xml  { render :xml => @status_setting.errors, :status => :unprocessable_entity }
+      end
+    end
+  end
+
+  # PUT /admin/status_settings/1
+  # PUT /admin/status_settings/1.xml
+  def update
+    @status_setting = StatusSetting.find(params[:id])
+
+    respond_to do |format|
+      if @status_setting.update_attributes(params[:status_setting])
+        flash[:notice] = 'status_setting was successfully updated.'
+        format.html { redirect_to(admin_status_setting_path(@status_setting)) }
+        format.xml  { head :ok }
+      else
+        format.html { render :action => "edit" }
+        format.xml  { render :xml => @status_setting.errors, :status => :unprocessable_entity }
+      end
+    end
+  end
+
+  # DELETE /admin/status_settings/1
+  # DELETE /admin/status_settings/1.xml
+  def destroy
+    @status_setting = StatusSetting.find(params[:id])
+    @status_setting.destroy
+
+    respond_to do |format|
+      format.html { redirect_to(admin_status_settings_path) }
+      format.xml  { head :ok }
+    end
+  end
+
+
 end
